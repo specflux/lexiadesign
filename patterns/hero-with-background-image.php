220a2e85c1a47ff99a071ffa245a8476353e6c04
Stephen Paul Samynathan <stephenpaul@specflux.com>
2024-11-13 14:40:14 +0800

    Added 3 hero patterns

---
+++ patterns/hero-with-background-image.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Title: Hero with background image
+ * Slug: lexiadesign/hero-with-background-image
+ * Description: Hero section with a background image.
+ * Categories: lexiadesign/hero, lexiadesign/call-to-action
+ * Keywords: cta, header, buttons, heading, hero, feature, homepage
+ * Viewport Width: 1500
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+?>
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/laptop-on-table.webp","id":1886,"dimRatio":50,"isUserOverlayColor":true,"customGradient":"linear-gradient(180deg,#006EF3 0%,#002A5D 100%)","metadata":{"categories":["lexiadesign/hero"],"patternName":"lexiadesign/hero-with-background-image","name":"Hero with background image"},"align":"full","style":{"color":{"duotone":"unset"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull"><span aria-hidden="true" class="wp-block-cover__background has-background-dim wp-block-cover__gradient-background has-background-gradient" style="background:linear-gradient(180deg,#006EF3 0%,#002A5D 100%)"></span><img class="wp-block-cover__image-background wp-image-1886" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/laptop-on-table.webp" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:spacer {"height":"var:preset|spacing|xx-large"} -->
+<div style="height:var(--wp--preset--spacing--xx-large)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-xx-large-font-size"><?php echo esc_html_e( 'Design with clicks, not code', 'lexiadesign' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"textColor":"main-accent","fontSize":"medium"} -->
+<p class="has-main-accent-color has-text-color has-medium-font-size"><?php echo esc_html_e( 'Whip up stunning, one-of-a-kind websites like a boss with the new WordPress Site Editor and the Lexia block theme!', 'lexiadesign' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"base","textColor":"main"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-main-color has-base-background-color has-text-color has-background wp-element-button"><?php echo esc_html_e( 'Tell Me More', 'lexiadesign' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|xx-large"} -->
+<div style="height:var(--wp--preset--spacing--xx-large)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div></div>
+<!-- /wp:cover -->
---
+++ patterns/hero-with-image-tiles.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Title: Hero with image tiles
+ * Slug: lexiadesign/hero-with-image-tiles
+ * Description: Hero section with image tiles
+ * Categories: lexiadesign/hero, lexiadesign/call-to-action
+ * Keywords: cta, header, buttons, heading, hero, feature, homepage
+ * Viewport Width: 1500
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"metadata":{"categories":["lexiadesign/hero"],"patternName":"lexiadesign/hero-with-image-tiles","name":"Hero with gallery image"},"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"backgroundColor":"brand-50"} -->
+<div class="wp-block-group alignfull has-brand-50-background-color has-background" style="padding-right:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:columns {"verticalAlignment":null,"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|medium"},"minHeight":"100%"},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontWeight":"300","fontStyle":"normal"}},"textColor":"primary"} -->
+<p class="has-primary-color has-text-color" style="font-style:normal;font-weight:300"><?php echo esc_html_e( 'Introducing', 'lexiadesign' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"style":{"typography":{"letterSpacing":"-1px"}},"fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-x-large-font-size" style="letter-spacing:-1px"><?php echo esc_html_e( 'Design with clicks, not code', 'lexiadesign' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"textColor":"secondary","fontSize":"medium"} -->
+<p class="has-secondary-color has-text-color has-medium-font-size"><?php echo esc_html_e( 'Whip up stunning, one-of-a-kind websites like a boss with the new WordPress Site Editor and the Lexia block theme!', 'lexiadesign' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"brand","textColor":"base-0","style":{"elements":{"link":{"color":{"text":"var:preset|color|base-0"}}},"border":{"radius":"15px"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-base-0-color has-brand-background-color has-text-color has-background has-link-color wp-element-button" style="border-radius:15px"><?php echo esc_html_e( 'Tell Me More', 'lexiadesign' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"className":"ollie-avatar-row","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+<div class="wp-block-group ollie-avatar-row"><!-- wp:image {"width":"60px","sizeSlug":"thumbnail","className":"is-resized","style":{"border":{"radius":"100px","width":"0px","style":"none"}}} -->
+<figure class="wp-block-image size-thumbnail is-resized has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/people-3.webp" alt="" style="border-style:none;border-width:0px;border-radius:100px;width:60px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"width":"60px","sizeSlug":"thumbnail","className":"is-resized","style":{"border":{"radius":"100px","width":"0px","style":"none"}}} -->
+<figure class="wp-block-image size-thumbnail is-resized has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/people-2.webp" alt="" style="border-style:none;border-width:0px;border-radius:100px;width:60px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"width":"60px","sizeSlug":"thumbnail","className":"is-resized","style":{"border":{"radius":"100px","width":"0px","style":"none"}}} -->
+<figure class="wp-block-image size-thumbnail is-resized has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/people-1.webp" alt="" style="border-style:none;border-width:0px;border-radius:100px;width:60px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"textColor":"brand-900"} -->
+<p class="has-brand-900-color has-text-color">★★★★★</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontWeight":"600","fontStyle":"normal"}},"textColor":"secondary","fontSize":"x-small"} -->
+<p class="has-secondary-color has-text-color has-x-small-font-size" style="font-style:normal;font-weight:600"><?php echo esc_html_e( 'Rated 5/5 stars by 1,234 happy reviewers', 'lexiadesign' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"50%","style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:image {"aspectRatio":"9/16","scale":"cover","sizeSlug":"large","style":{"border":{"radius":"30px"}}} -->
+<figure class="wp-block-image size-large has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/people-4.webp" alt="" style="border-radius:30px;aspect-ratio:9/16;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:image {"aspectRatio":"9/16","scale":"cover","sizeSlug":"large","style":{"border":{"radius":"30px"}}} -->
+<figure class="wp-block-image size-large has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/people-4.webp" alt="" style="border-radius:30px;aspect-ratio:9/16;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"aspectRatio":"9/16","scale":"cover","sizeSlug":"large","style":{"border":{"radius":"30px"}}} -->
+<figure class="wp-block-image size-large has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/people-4.webp" alt="" style="border-radius:30px;aspect-ratio:9/16;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:image {"aspectRatio":"9/16","scale":"cover","sizeSlug":"large","style":{"border":{"radius":"30px"}}} -->
+<figure class="wp-block-image size-large has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/people-4.webp" alt="" style="border-radius:30px;aspect-ratio:9/16;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"aspectRatio":"9/16","scale":"cover","sizeSlug":"large","style":{"border":{"radius":"30px"}}} -->
+<figure class="wp-block-image size-large has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/people-4.webp" alt="" style="border-radius:30px;aspect-ratio:9/16;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"aspectRatio":"9/16","scale":"cover","sizeSlug":"large","style":{"border":{"radius":"30px"}}} -->
+<figure class="wp-block-image size-large has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/people-4.webp" alt="" style="border-radius:30px;aspect-ratio:9/16;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
